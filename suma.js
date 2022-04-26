de3e57432c0daeb6858c54aa7b91cddf9ac3909c
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2022-04-26 18:12:18 -0500

    Add files via upload

---
+++ suma.js
@@ -0,0 +1,10 @@
+let suma=0
+ 
+for(let i=0; i<5000; i++){
+    if(i%2==0){
+        suma=(suma+(1/((2*i)+1))*4)
+    }else{
+        suma=(suma-(1/((2*i)+1))*4)
+    }
+}
+console.log("La suma es "+suma)
